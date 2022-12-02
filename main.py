51116db9569f92eecaa38c89579139632921b91f
FinoanaRandria <niriantsoa.randriambololomanana@esti.mg>
2022-12-02 18:56:39 +0300

    test microphone

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ listener = sr.Recognizer()
 try:
     with sr.Microphone() as source:
         print('Parler maintenant')
+        voix = listener.listen(source)
+        command = listener.recognize_google(voix)
+        print(command)
+except:
+    pass
